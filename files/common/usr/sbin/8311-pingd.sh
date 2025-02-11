726d767191086f6f99af97c5f4bc062d7593122b
Killer_Z <3115216193@qq.com>
2025-02-11 16:48:11 +0800

    pingd重启接口5次限制，优化中文翻译

--- files/common/usr/sbin/8311-pingd.sh
+++ files/common/usr/sbin/8311-pingd.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 _lib_8311 2>/dev/null || . /lib/8311.sh
-
+restart_Num=0
 while true ; do
 	PINGD_ENABLED=$(fwenv_get_8311 "pingd" "1")
 	# Ping host to help management work
@@ -10,16 +10,15 @@ while true ; do
 	echo "Starting ping to: $PING_HOST" | to_console
 
 	if [ "$PINGD_ENABLED" -ne "0" ] 2>/dev/null; then
-		ping -i 5 "$PING_HOST" &> /dev/null < /dev/null
-		ping_pid=$!
-		# 等待指定 PID 的进程完成
-		wait $ping_pid
+		ping -i 1 -c 3 "$PING_HOST" &> /dev/null < /dev/null
 		# 获取返回值（0=成功，非0=失败）
 		ping_result=$?
-		if [ "$ping_result" -eq 1 ]; then
+		if [ "$ping_result" -eq 1 ] && [ "$restart_Num" -le 5 ]; then
 			echo "100% packet loss detected,try to restart lct port" | to_console
 			ifup "lct"
 			[ "$LCT_VLAN" -gt 0 ] && ifup "mgmt"
+			restart_Num=$((restart_Num+1))
+			sleep 5
 		fi
 
 		sleep 5
