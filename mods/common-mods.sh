8eb41bfc3d37df0e2d452bf80299e4a90dd663af
xiao-k233 <3115216193@qq.com>
2025-04-05 13:32:02 +0800

    Update Submodule Fix&Feat:干掉dectvlan；修复多播转换不生效

--- mods/common-mods.sh
+++ mods/common-mods.sh
@@ -96,9 +96,8 @@ PONSHMAC
 echo "$LIB_PON_SH_FOOT" >> "$LIB_PON_SH"
 
 
-cp -fv "8311-xgspon-bypass/8311-detect-config.sh" "8311-xgspon-bypass/8311-fix-vlans.sh" "$ROOT_DIR/usr/sbin/"
+cp -fv "8311-xgspon-bypass/8311-fix-vlans.sh" "$ROOT_DIR/usr/sbin/"
 
-ln -fsv "/usr/sbin/8311-detect-config.sh" "$ROOT_DIR/root/8311-detect-config.sh"
 ln -fsv "/usr/sbin/8311-fix-vlans.sh" "$ROOT_DIR/root/8311-fix-vlans.sh"
 
 mkdir -p "$ROOT_DIR/etc/crontabs"
